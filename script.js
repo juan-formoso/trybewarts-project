b52d286f8ed519315b99a8f9885c8819dd0b48f2
Juan Formoso <trajano.juan@gmail.com>
2021-07-22 20:21:32 -0400

    Finalizando requisito 21

--- script.js
+++ script.js
@@ -33,3 +33,93 @@ function count() {
 }
 
 textarea.addEventListener('input', count);
+
+const evaluationForm = document.getElementById('evaluation-form');
+const formEmail = document.getElementById('input-email');
+
+function collectName() {
+  const lastName = document.getElementById('input-lastname').value;
+  const firstName = document.getElementById('input-name').value;
+  const fullName = `Nome: ${firstName} ${lastName}`;
+  evaluationForm.innerHTML = '';
+  const paragraph = document.createElement('p');
+  const son = evaluationForm.appendChild(paragraph);
+  son.innerText = fullName;
+}
+
+function collectEmail() {
+  const email = `Email: ${formEmail.value}`;
+  const paragraph = document.createElement('p');
+  const son = evaluationForm.appendChild(paragraph);
+  son.innerText = email;
+}
+
+const selectedHouse = document.getElementById('house');
+
+function collectHouse() {
+  const house = `Casa: ${selectedHouse.value}`;
+  const paragraph = document.createElement('p');
+  const son = evaluationForm.appendChild(paragraph);
+  son.innerText = house;
+}
+
+const selectedFamily = document.querySelectorAll('.family');
+
+function collectFamily() {
+  const paragraph = document.createElement('p');
+  const son = evaluationForm.appendChild(paragraph);
+  for (let index = 0; index < selectedFamily.length; index += 1) {
+    if (selectedFamily[index].checked) {
+      const family = selectedFamily[index].value;
+      son.innerText = `Família: ${family}`;
+    }
+  }
+}
+
+const selectedNotes = document.querySelectorAll('input[name="rate"]');
+
+function collectNotes() {
+  const paragraph = document.createElement('p');
+  const son = evaluationForm.appendChild(paragraph);
+  for (let index = 0; index < selectedNotes.length; index += 1) {
+    if (selectedNotes[index].checked) {
+      const Notes = selectedNotes[index].value;
+      son.innerText = `Avaliação: ${Notes}`;
+    }
+  }
+}
+
+const selectedSubjects = document.querySelectorAll('.subject');
+
+function collectSubjects() {
+  const paragraph = document.createElement('p');
+  const son = evaluationForm.appendChild(paragraph);
+  son.innerText = 'Matérias: ';
+  for (let index = 0; index < selectedSubjects.length; index += 1) {
+    if (selectedSubjects[index].checked) {
+      son.innerText += ` ${selectedSubjects[index].value},`;
+    }
+  }
+}
+
+const observations = document.getElementById('textarea');
+
+function collectObservations() {
+  const writtenObs = `Observações: ${observations.value}`;
+  const paragraph = document.createElement('p');
+  const son = evaluationForm.appendChild(paragraph);
+  son.innerText = writtenObs;
+}
+
+function collectData(event) {
+  event.preventDefault();
+  collectName();
+  collectEmail();
+  collectHouse();
+  collectFamily();
+  collectNotes();
+  collectSubjects();
+  collectObservations();
+}
+
+submitBtn.addEventListener('click', collectData);
